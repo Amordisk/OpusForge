1c4325e09d284bb2814a246b74c81c21f1f92475
disk <galaxiangamerx@gmail.com>
2024-03-04 10:30:58 -0500

    First item and item registries

--- src/main/java/com/disk/opus/Opus.java
+++ src/main/java/com/disk/opus/Opus.java
@@ -2,11 +2,14 @@ package com.disk.opus;
 
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.disk.opus.item.ModItems;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Opus.MOD_ID)
@@ -18,8 +21,13 @@ public class Opus
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Opus() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Item Register stuff
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
         
 
         // Register ourselves for server and other game events we are interested in
---
+++ src/main/java/com/disk/opus/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.disk.opus.item;
+
+import com.disk.opus.Opus;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = 
+            DeferredRegister.create(ForgeRegistries.ITEMS, Opus.MOD_ID);
+
+
+    // Items
+    @SuppressWarnings("null")
+    public static final RegistryObject<Item> TEST_TUBE = ITEMS.register("test_tube",
+     () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
