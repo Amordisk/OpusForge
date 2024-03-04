6ea9040753bbb935096360349d3107b5c7d4a023
Ruben Huizenga <RubenHuizenga@proton.me>
2024-03-04 23:26:12 +0100

    Fixed broken block registration

--- src/main/java/com/disk/opus/block/ModBlocks.java
+++ src/main/java/com/disk/opus/block/ModBlocks.java
@@ -16,7 +16,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 @SuppressWarnings("null")
-public class ModBlocks {
+public class ModBlocks 
+{
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Opus.MOD_ID);
 
     //#region Blocks
@@ -29,7 +30,7 @@ public class ModBlocks {
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) 
     {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+        ModItems.ITEMS.register(name, () -> new BlockItem(toReturn.get(), new Item.Properties().tab(tab)));
         return toReturn;
     }
 
