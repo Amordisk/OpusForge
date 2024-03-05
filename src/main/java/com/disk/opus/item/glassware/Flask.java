87d423a927a14118e06f6d434981a40d57eee8f7
Disk <galaxiangamerx@gmail.com>
2024-03-05 18:29:52 -0500

    added water bottle logic

--- src/main/java/com/disk/opus/item/glassware/Flask.java
+++ src/main/java/com/disk/opus/item/glassware/Flask.java
@@ -1,6 +1,24 @@
 package com.disk.opus.item.glassware;
 
+import javax.annotation.Nonnull;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.stats.Stats;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 
 public class Flask extends Item
 {
@@ -8,5 +26,37 @@ public class Flask extends Item
     {
         super(properties);
     }
-
+    
+    @Override
+    public InteractionResultHolder<ItemStack> use( @Nonnull Level world, @Nonnull Player player, @Nonnull InteractionHand hand)
+    {
+        ItemStack itemstack = player.getItemInHand(hand);
+        HitResult hitresult = getPlayerPOVHitResult(world, player, ClipContext.Fluid.SOURCE_ONLY);
+        if (hitresult.getType() == HitResult.Type.MISS)
+        {
+            return InteractionResultHolder.pass(itemstack);
+        }
+        else
+        {
+            if (hitresult.getType() == HitResult.Type.BLOCK)
+            {
+                BlockPos blockpos = ((BlockHitResult)hitresult).getBlockPos();
+                if (!world.mayInteract(player, blockpos))
+                {
+                    return InteractionResultHolder.pass(itemstack);
+                }
+                if (world.getFluidState(blockpos).is(FluidTags.WATER))
+                {
+                    world.gameEvent(player, GameEvent.FLUID_PICKUP, blockpos);
+                    return InteractionResultHolder.sidedSuccess(this.turnBottleIntoItem(itemstack, player, PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.WATER)), world.isClientSide());
+                }
+            }
+            return InteractionResultHolder.pass(itemstack);
+        }
+    }
+    protected ItemStack turnBottleIntoItem(ItemStack itemstack, Player player, ItemStack seconditemstack)
+    {
+        player.awardStat(Stats.ITEM_USED.get(this));
+        return ItemUtils.createFilledResult(itemstack, player, seconditemstack);
+    }
 }
