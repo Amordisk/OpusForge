d6151effc4bb82523702f81a3e8392128f5ab7f5
Ruben Huizenga <RubenHuizenga@proton.me>
2024-03-05 23:54:10 +0100

    Test unit test failure fails Pull Request

--- src/test/java/com/disk/opus/item/Tests.java
+++ src/test/java/com/disk/opus/item/Tests.java
@@ -9,7 +9,7 @@ public class Tests
     @Test
     public void testAdd()
     {
-        final var expected = 42;
+        final var expected = -1;
         final var a = 19;
         final var b = 23;
         assertEquals(expected, Integer.sum(a, b));
